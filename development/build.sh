1ee43131a0f587e7b3abb56237a56601550781c6
kkaneko <kkaneko@student.42tokyo.jp>
2022-04-16 03:25:37 +0900

    build.sh modified

--- development/build.sh
+++ development/build.sh
@@ -7,6 +7,7 @@
 # web画面の変更を反映したい場合、コメントアウトを外す。
 # (cd ./frontend && npm run build)
 
+docker rm -f `docker ps -a -q`
 docker-compose down --rmi all
 docker-compose build --no-cache
 docker-compose up -d
