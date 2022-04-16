33440b214ebf70a39a0c98d473c27a4a6b13144c
taisey <taisey02@gmail.com>
2022-04-16 21:07:02 +0900

    add

--- development/backend/src/api.js
+++ development/backend/src/api.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-
+const performance = require('perf_hooks').performance;
 const jimp = require('jimp');
 
 const mysql = require('mysql2/promise');
